2b4eb09e5a16e46bf39a682517a01cbcc165e567
laislinda1 <119938559+laislinda1@users.noreply.github.com>
2024-10-07 15:41:30 +0000

    aula 07-10

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,3 +1,20 @@
-export default function navbar() {
-    return <div>Menu</div>
+import {Container, Nav, Navbar} from 'react-bootstrap';
+
+
+export default function Menu() {
+  return (
+    <Navbar expand="sn" className="grad" >
+      <Container>
+        <Navbar.Brand href="/">Lais Linda</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,23 @@
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-function Index() {
+import {Container, Row, Col} from 'react-bootstrap';
+
+export default function Index() {
   return <div>
 
     <Navbar />
     <Container>
       <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
+        <Col className="bg-info-subtle">1 of 2</Col>
+        <Col className="bg-info-subtle">2 of 2</Col>
       </Row>
       <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col className="bg-info-subtle">1 of 3</Col>
+        <Col className="bg-info-subtle">2 of 3</Col>
+        <Col className="bg-info-subtle">3 of 3</Col>
       </Row>
     </Container>
     <Footer />
   </div>;
 
 }
-export default Index; 
